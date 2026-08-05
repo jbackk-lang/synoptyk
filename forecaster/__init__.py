ce8903868da2e005d673dd83262b6f0cbd5a7202
Jacek Kielich <jbackk@gmail.com>
2026-08-05 11:31:06 +0200

    Update __init__.py

--- forecaster/__init__.py
+++ forecaster/__init__.py
@@ -1,5 +1,5 @@
 # forecaster/__init__.py
-from .synoptic_f import synoptic_f
+from .synoptic_f import SynopticF
 from .validator import ForecastValidator
 
 __all__ = ["SynopticF", "ForecastValidator"]
